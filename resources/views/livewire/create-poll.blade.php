3628847d99515f06237cf84ec3b6c3ed21cce03c
hiiamhip <trantuanhiep420@gmail.com>
2025-06-10 11:35:41 +0000

    first commit

---
+++ resources/views/livewire/create-poll.blade.php
@@ -0,0 +1,25 @@
+<div>
+    <form wire:submit.prevent="createPoll">
+        <label for="">POLL TITLE</label>
+
+        <input type="text" wire:model="title">
+
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+        </div>
+
+        <div>
+            @foreach ($options as $index => $option)
+                <div class="mb-4">
+                    <label for="">Option {{ $index + 1 }}</label>
+                    <div class="flex gap-2">
+                        <input type="text" wire:model="options.{{ $index }}">
+                        <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+        <button type="submit" class="btn">Create Poll</button>
+    </form>
+</div>
